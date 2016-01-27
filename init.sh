5db56fdfcdeceed56e007d395a06e97a7f5c243b
petecarapetyan <pete@couldbe.net>
2016-01-27 10:30:09 -0600

    first attempt to include vagrant up

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 echo "NOTE: for future removal - exports and folder are prefixed rm_pc or RM_PC for easy identification and removal"
 mkdir ~/rm_pc
+mkdir ~/rm_pc/work
 cd ~/rm_pc
 git clone https://github.com/petecarapetyan/contrasting.git
 git clone https://github.com/petecarapetyan/precontrast.git
@@ -13,3 +14,9 @@ export RM_PC_DOCKER_MYSQL_PORT=blah
 export RM_PC_DOCKER_MYSQL_PORT=blah
 export RM_PC_REMOTE_REST=http://172.99.79.10:8123/
 export RM_PC_DOCKER_=blah
+#printenv
+cd ~/rm_pc/contrasting
+mvn clean install -DskipTests
+source ~/rm_pc/contrasting/copytodockervm.sh
+cd ~/rm_pc/precontrast_docker_vagrant
+vagrant up
